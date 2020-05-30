3f79b38720bd87d24f0e94b2a2d9a41cd378523e
jxu <7989982+jxu@users.noreply.github.com>
2020-05-30 17:48:21 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,208 @@
+var CONFIG = {
+  "maps": {
+    "school_1.12.2_iso": {
+      "defaultView": [
+        0,
+        0,
+        64
+      ],
+      "defaultZoom": 4,
+      "imageFormat": "png",
+      "lastRendered": [
+        1590870920,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 8,
+      "name": "School 1.12.2 isometric",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 4,
+      "tileSetGroup": "school_1.12.2_isometric_t4",
+      "tileSize": 512,
+      "world": "school_1.12.2",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    },
+    "school_1.12.2_top": {
+      "defaultView": [
+        0,
+        0,
+        64
+      ],
+      "defaultZoom": 4,
+      "imageFormat": "png",
+      "lastRendered": [
+        1590870920,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 8,
+      "name": "School 1.12.2 top-down",
+      "renderView": "topdown",
+      "rotations": [
+        0
+      ],
+      "textureSize": 4,
+      "tileSetGroup": "school_1.12.2_topdown_t4",
+      "tileSize": 256,
+      "world": "school_1.12.2",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    },
+    "school_1.7.9_iso": {
+      "defaultView": [
+        0,
+        0,
+        64
+      ],
+      "defaultZoom": 4,
+      "imageFormat": "png",
+      "lastRendered": [
+        1590870920,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 7,
+      "name": "School 1.7.9 isometric",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 4,
+      "tileSetGroup": "school_1.7.9_isometric_t4",
+      "tileSize": 512,
+      "world": "school_1.7.9",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    },
+    "school_1.7.9_top": {
+      "defaultView": [
+        0,
+        0,
+        64
+      ],
+      "defaultZoom": 4,
+      "imageFormat": "png",
+      "lastRendered": [
+        1590870920,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 7,
+      "name": "School 1.7.9 top-down",
+      "renderView": "topdown",
+      "rotations": [
+        0
+      ],
+      "textureSize": 4,
+      "tileSetGroup": "school_1.7.9_topdown_t4",
+      "tileSize": 256,
+      "world": "school_1.7.9",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "school_1.7.9_top",
+    "school_1.7.9_iso",
+    "school_1.12.2_top",
+    "school_1.12.2_iso"
+  ],
+  "tileSetGroups": {
+    "school_1.12.2_isometric_t4": {
+      "maxZoom": 8,
+      "tileOffsets": [
+        [
+          -1,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 4
+    },
+    "school_1.12.2_topdown_t4": {
+      "maxZoom": 8,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 4
+    },
+    "school_1.7.9_isometric_t4": {
+      "maxZoom": 7,
+      "tileOffsets": [
+        [
+          -1,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 4
+    },
+    "school_1.7.9_topdown_t4": {
+      "maxZoom": 7,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 4
+    }
+  }
+};
